f5899ca6e1af3aa42c3a32d2a35c2b3d386b983a
Ibad Saladdin <ibadsaladdin@gmail.com>
2019-04-07 23:45:48 +0700

    remove zopfli and test

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -3,5 +3,6 @@ set -x
 # Build the site with Jekyll
 bundle exec jekyll build
 
-# Compress assets with Zopfli
-zopfli/zopfli --i1000 _site/**/*.html _site/*.html  _site/**/*.css _site/*.css _site/**/*.js _site/*.js _site/**/*.xml _site/*.xml
+# Compress assets with Zopfli 
+# SKIPPED
+#zopfli/zopfli --i1000 _site/**/*.html _site/*.html  _site/**/*.css _site/*.css _site/**/*.js _site/*.js _site/**/*.xml _site/*.xml
--- _scripts/test.sh
+++ _scripts/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-set -x
-
-timeout 30s bundle exec htmlproofer _site --only-4xx --external_only --check-html --check-favicon --allow-hash-href --http-status-ignore 429
+#set -x
+#SKIPPED
+#timeout 30s bundle exec htmlproofer _site --only-4xx --external_only --check-html --check-favicon --allow-hash-href --http-status-ignore 429
